1c1b5bdd75941dca5c656afb46163023cf995b8d
Mach3b3 <lethumachebe@gmail.com>
2024-09-13 14:13:32 +0200

    deleted image

--- lib/views/weather_details.dart
+++ lib/views/weather_details.dart
@@ -15,6 +15,7 @@ class WeatherDetails extends StatefulWidget {
   });
 
   @override
+  // ignore: library_private_types_in_public_api
   _WeatherDetailsState createState() => _WeatherDetailsState();
 }
 
